c2b8ee50f7b1b890d0d70dddbc238cdaa105f9a8
Artem <artchzhen@gmail.com>
2023-04-14 16:45:03 +0900

    initial commit devolopment

---
+++ GCD project9/GCD project9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD project9
+//
+//  Created by Артем Чжен on 14/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
